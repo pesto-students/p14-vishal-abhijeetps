7485230941a9a9ba93e6327a0f46baae827d63c4
Abhijeet Singh <abhijeetps@outlook.com>
2023-10-22 20:45:36 +0530

    feat: add isStrongPassword function

--- Week-3/JavaScript password strength/script.js
+++ Week-3/JavaScript password strength/script.js
@@ -1,1 +1,9 @@
 // Your solution goes here
+const is8Char = (password) => password.length >= 8;
+
+const has1UpperCase = (password) => /[A-Z]/.test(password);
+
+const isPasswordString = (password) => password.includes('password');
+
+const isStrongPassword = (password) =>
+  is8Char(password) && has1UpperCase(password) && !isPasswordString(password);

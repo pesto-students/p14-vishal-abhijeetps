200f592fa6b3bb13df9e340b30ad77c3f5756cbd
Abhijeet Singh <abhijeetps@outlook.com>
2023-10-22 17:42:46 +0530

    feat: add divideArray function

--- Week-3/JavaScript arrays/script.js
+++ Week-3/JavaScript arrays/script.js
@@ -1,1 +1,33 @@
 // Put your solution here
+
+const printArray = (arrayType, arr) => {
+  if (arrayType === 'odd') {
+    console.log('Odd numbers: ');
+  } else if (arrayType === 'even') {
+    console.log('Even numbers: ');
+  }
+  if (arr.length === 0) {
+    console.log('None');
+    return;
+  }
+
+  arr.forEach((num) => {
+    console.log(num);
+  });
+};
+
+const divideArray = (arr) => {
+  const evenNums = [];
+  const oddNums = [];
+  arr.forEach((num) => {
+    if (num % 2 === 0) {
+      evenNums.push(num);
+    } else {
+      oddNums.push(num);
+    }
+  });
+  evenNums.sort((a, b) => a - b);
+  oddNums.sort((a, b) => a - b);
+  printArray('even', evenNums);
+  printArray('odd', oddNums);
+};

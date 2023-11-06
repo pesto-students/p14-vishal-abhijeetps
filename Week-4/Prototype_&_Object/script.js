b3d740a0a566201434ed4a28d6261fa733f2341b
Abhijeet Singh <abhijeetps@outlook.com>
2023-11-06 18:39:44 +0530

    feat: add javascript prototype

--- Week-4/Prototype_&_Object/script.js
+++ Week-4/Prototype_&_Object/script.js
@@ -18,3 +18,29 @@ person.setAge = function (age) {
 console.log(person.getAge());
 person.setAge(27);
 console.log(person.getAge());
+
+function Vehicle(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+
+Vehicle.prototype.getDetails = function () {
+  return `${this.make} ${this.model} ${this.year}.`;
+};
+
+function Car(make, model, year, numDoors) {
+  Vehicle.call(this, make, model, year);
+  this.numDoors = numDoors;
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.getDetails = function () {
+  return `${this.make} ${this.model} ${this.year} ${this.numDoors}.`;
+};
+
+const vehicle = new Vehicle('Tata', 'Sumo', 2010);
+const car = new Car('Tata', 'Nexon', 2022, 5);
+
+console.log(vehicle.getDetails());
+console.log(car.getDetails());

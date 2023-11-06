a54aed757fc5bcf3fe04f44c1848c5b4569a7e33
Abhijeet Singh <abhijeetps@outlook.com>
2023-11-06 16:14:52 +0530

    feat: implemented inheritance assignment

---
+++ Week-4/OOPS_Practice_ES6/script.js
@@ -0,0 +1,26 @@
+class Vehicle {
+  constructor(make, model, year, color) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
+  drive() {
+    console.log(`Driving ${this.make} ${this.model}.`);
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, model, year, color, numSeats) {
+    super(make, model, year, color);
+    this.numSeats = numSeats;
+  }
+}
+
+class RideShareCar extends Car {
+  constructor(make, model, year, color, numSeats, isAvailable) {
+    super(make, model, year, color, numSeats);
+    this.isAvailable = isAvailable;
+  }
+}
+
